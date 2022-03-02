d9da56bec1389005618c981acbfb1abf18a6f8a2
Ivan Llamas <ip.llamas13@gmail.com>
2022-03-02 02:44:36 -0800

    Restore from backup Previous progress was corrupted, restoring from backup Changes include: functional buttons, display functionality, and updated styling. Script file also has some groundwork laid for making use of the math operations

--- script.js
+++ script.js
@@ -1,3 +1,92 @@
+const numpadButtons = document.querySelectorAll(".numpad, .operations");
+numpadButtons.forEach((btn) => btn.addEventListener('click', processInput));
+
+let currentDisplay = "0";
+let operand = currentDisplay;
+let operator;
+const displayText = document.querySelector('.display-text');
+const operationsText = document.querySelector('.operations-text');
+
+
+function processInput(btn) {
+  const pressed = btn.target.getAttribute('class');
+  console.log(pressed);
+  pressed === 'numpad' ? inputDigit(btn.target) : inputOperator(btn.target);
+}
+
+function inputDigit(pressedDigit) {
+  const digit = pressedDigit.getAttribute('id').slice(6);
+  console.log(digit);
+  if (currentDisplay === '0') {
+    changeDisplay(digit, true);
+  } else if (digit === '.' && currentDisplay.includes('.')) {
+    return;
+  } else if (isNaN(currentDisplay) && !(currentDisplay.includes('.'))) {
+    changeDisplay(digit, true);
+  } else {
+    changeDisplay(digit, false);
+  }
+  operand = Number(currentDisplay);
+  console.log(operand);
+}
+
+
+function inputOperator(pressedOperator) {
+  operator = pressedOperator.getAttribute('id');
+  switch (operator) {
+    case 'clear':
+      clearDisplay();
+      break;
+    case 'add':
+      changeDisplay('+', true, true);
+      break;
+    case 'subtract':
+      changeDisplay('-', true, true);
+      break;
+    case 'multiply':
+      changeDisplay('x', true, true);
+      break
+    case 'divide':
+      changeDisplay('/', true, true);
+      break
+    case 'delete':
+      deleteFromDisplay();
+      break;
+    case 'equals':
+      
+  }
+}
+
+//Changes display, overriding current display if needed(ie replace "+" with inputted numbers)
+function changeDisplay(input, allowOverride, isOperator=false){
+  allowOverride ? currentDisplay = input : currentDisplay += input;
+  displayText.textContent = currentDisplay;
+
+  if (isOperator) {
+    operationsText.textContent = `${operand} ${currentDisplay}`;
+  }    
+}
+
+function deleteFromDisplay() {
+  if (isNaN(currentDisplay) && !(currentDisplay.includes('.'))){
+    currentDisplay = operand;
+  } else if (currentDisplay.length >= 2) {
+    currentDisplay = currentDisplay.slice(0 ,-1);
+  } else {
+    currentDisplay = '0';
+  }
+  operand = currentDisplay;
+  displayText.textContent = currentDisplay;
+}
+
+function clearDisplay() {
+  currentDisplay = '0';
+  operand = currentDisplay;
+  operator = undefined;
+  displayText.textContent = currentDisplay;
+  operationsText.textContent = '';
+}
+
 function add(a, b) {
   return a+b;
 }
