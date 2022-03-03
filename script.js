5bb559b6fcb91335ab97cb474873253107e196bc
Ivan Llamas <ip.llamas13@gmail.com>
2022-03-02 17:40:50 -0800

    include working equals sign to correctly output expected result

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ numpadButtons.forEach((btn) => btn.addEventListener('click', processInput));
 let currentDisplay = "0";
 let operand = currentDisplay;
 let operator;
+
 const displayText = document.querySelector('.display-text');
 const operationsText = document.querySelector('.operations-text');
 
 
 function processInput(btn) {
   const pressed = btn.target.getAttribute('class');
-  console.log(pressed);
   pressed === 'numpad' ? inputDigit(btn.target) : inputOperator(btn.target);
 }
 
@@ -26,39 +26,70 @@ function inputDigit(pressedDigit) {
   } else {
     changeDisplay(digit, false);
   }
-  operand = Number(currentDisplay);
-  console.log(operand);
 }
 
 
 function inputOperator(pressedOperator) {
-  operator = pressedOperator.getAttribute('id');
-  switch (operator) {
+  const pressed = pressedOperator.getAttribute('id');
+  console.log(pressed);
+  switch (pressed) {
     case 'clear':
       clearDisplay();
       break;
     case 'add':
+      if (!operator) {
+        operand = Number(currentDisplay);
+      }
+      operator = pressed;
       changeDisplay('+', true, true);
       break;
     case 'subtract':
+      if (!operator) {
+        operand = Number(currentDisplay);
+      }
+      operator = pressed;
       changeDisplay('-', true, true);
       break;
     case 'multiply':
+      if (!operator) {
+        operand = Number(currentDisplay);
+      }
+      operator = pressed;
+      
       changeDisplay('x', true, true);
       break
     case 'divide':
+      if (!operator) {
+        operand = Number(currentDisplay);
+      }
+      operator = pressed;
+      
       changeDisplay('/', true, true);
       break
     case 'delete':
       deleteFromDisplay();
       break;
     case 'equals':
-      
+      console.log(operand)
+      console.log(currentDisplay);
+      console.log(operator);
+      doOperation();
+      /* const operand2 = Number(currentDisplay);
+      if (operator && !isNaN(currentDisplay)) {
+        let result = operate(operator, operand, operand2);
+        result = roundToTwoDecimals(result);
+        changeDisplay(String(result), true);
+        operand = result;
+        operator = undefined;
+        operationsText.textContent = '';
+      } */
+      break;
   }
 }
 
 //Changes display, overriding current display if needed(ie replace "+" with inputted numbers)
 function changeDisplay(input, allowOverride, isOperator=false){
+  
   allowOverride ? currentDisplay = input : currentDisplay += input;
   displayText.textContent = currentDisplay;
 
@@ -75,7 +106,6 @@ function deleteFromDisplay() {
   } else {
     currentDisplay = '0';
   }
-  operand = currentDisplay;
   displayText.textContent = currentDisplay;
 }
 
@@ -122,8 +152,18 @@ function operate(operation, a, b) {
   return result;
 }
 
-//Testing operate function with all operations
-/* console.log(operate('add', 3, 1));
-console.log(operate('subtract', 4, 1));
-console.log(operate('multiply', 1, 2));
-console.log(operate('divide', 1, 1)); */
+function doOperation() {
+  const operand2 = Number(currentDisplay);
+  if (operator && !isNaN(currentDisplay)) {
+    let result = operate(operator, operand, operand2);
+    result = roundToTwoDecimals(result);
+    changeDisplay(String(result), true);
+    operand = result;
+    operator = undefined;
+    operationsText.textContent = '';
+  }
+}
+
+function roundToTwoDecimals(num) {
+  return Math.round(num*100)/100
+}
